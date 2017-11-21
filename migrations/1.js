678093564c75bfcc60d1d12b90a14a5794d3f084
vgorin <vgrn.at@gmail.com>
2017-11-22 01:32:08 +0200

    remove migrations

--- migrations/1.js
+++ migrations/1.js
@@ -1,4 +1,4 @@
-module.exports = function(deployer, network) {
+module.exports = function(deployer, network, accounts) {
 	var Protocol108 = artifacts.require("./Protocol108.sol");
 
 	deployer.deploy(Protocol108);
