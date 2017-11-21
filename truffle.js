f38bdf5ce3ea11ebf9afa0094c748be8829cda83
vgorin <vgrn.at@gmail.com>
2017-11-21 20:23:43 +0200

    truffle settings file refactoring

--- truffle.js
+++ truffle.js
@@ -1,24 +1,25 @@
 module.exports = {
   networks: {
     main: {
-      host: "localhost",
-      port: 8545,
-      network_id: "1"
+      host: "localhost"
+      ,port: 8545
+      ,network_id: "1"
     },
     ropsten: {
-      host: "localhost",
-      port: 8545,
-      network_id: "3"
+      host: "localhost"
+      ,port: 8545
+      ,network_id: "3"
     },
     rinkeby: {
-      host: "localhost",
-      port: 8545,
-      network_id: "4"
+      host: "localhost"
+      ,port: 8545
+      ,network_id: "4"
+      // ,gasPrice: 1
     },
     kovan: {
-      host: "localhost",
-      port: 8545,
-      network_id: "42"
+      host: "localhost"
+      ,port: 8545
+      ,network_id: "42"
     }
   }
 };
