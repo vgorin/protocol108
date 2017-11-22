61a4bdfb59cc6014034ae92486ec44d0f94664e5
vgorin <vgrn.at@gmail.com>
2017-11-22 02:09:40 +0200

    provide dummy migrations contract

---
+++ contracts/Migrations.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.4.0;
+
+
+// dummy migrations contract
+contract Migrations {
+	function Migrations() {}
+
+	// A function with the signature `setCompleted(uint)` is required.
+	function setCompleted(uint completed) {}
+}
