38a763af535939049b7c5e9a8aef8ac433d5b7f9
vgorin <vgrn.at@gmail.com>
2017-12-06 11:58:12 +0200

    fix deployment scripts to be able to run simultaneously

--- rinkeby.sh
+++ rinkeby.sh
@@ -5,5 +5,6 @@ geth \
 --fast \
 --rpc \
 --rpcport 8544 \
+--port 38544 \
 --unlock 0x03cda1f3deeae2de4c73cfc4b93d3a50d0419c24 \
 --password <(echo Welcome1)
--- ropsten.sh
+++ ropsten.sh
@@ -5,5 +5,6 @@ geth \
 --fast \
 --rpc \
 --rpcport 8543 \
+--port 38543 \
 --unlock 0x03cda1f3deeae2de4c73cfc4b93d3a50d0419c24 \
 --password <(echo Welcome1)
